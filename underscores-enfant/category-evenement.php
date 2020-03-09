4d381fd35c82931cf239ea0f37e9f72a4ad1fd4c
Miguel Santana <miguelsantana@hotmail.ca>
2020-03-09 18:32:06 -0400

    Grid fixe

--- underscores-enfant/category-evenement.php
+++ underscores-enfant/category-evenement.php
@@ -2,19 +2,19 @@
 get_header();
 ?>
 <div id="primary" class="content-area">
-    <h2>Nos événements importants cette année</h2>
-    <main id="main" class="grille">
-    <?php
-    
-    while ( have_posts() ):
-        the_post();
-        $jour = get_the_date("j");
-        $mois = (int)get_the_date("m");
-        $gridArea = $jour. '/' . ($mois%3+1) . '/' . ($jour+1) . '/' . (($mois%3+1)+1);
+    <h1> Nos événements importants cette année </h1>
+    <main id="main" class ="site-main" style="display: grid; grid-template-columns:repeat(3,1fr); grid-template-rows:repeat(31, 1fr);" >
 
-        echo "<h3 style='grid-area:". $gridArea . "'>" . get_the_title() . " " . get_the_date('j m y') . "</h3>";
+<?php
 
-    endwhile;
-    ?>
-    </main>
+while (have_posts()) :
+    the_post();
+    $jour = get_the_date('j');
+    $mois = (int)get_the_date('m');
+    $gridArea = $jour. '/' . ($mois%3+1) . '/' . ($jour+1) . '/' . (($mois%3+1)+1);
+
+    echo "<h3 style ='grid-area:". $gridArea."'>".get_the_title() . " " . get_the_date('j m y') . "</h3>";
+endwhile;
+?>
+</main>
 </div>
